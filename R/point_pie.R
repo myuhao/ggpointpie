626de9c1794acb0993cfbf0c458e98542b20de07
Yuhao Min <myuhao@gmail.com>
2022-10-24 18:20:12 -0400

    A somewhat working version of pieGrob with snpc

--- R/point_pie.R
+++ R/point_pie.R
@@ -21,12 +21,12 @@
 #'
 #' @export
 geom_point_pie <- function(
-    mapping = NULL, data = NULL, stat = "identity",
+    mapping = NULL, data = NULL, stat = "point_pie",
     position = "identity", r0 = 0L, na.rm = FALSE, show.legend = NA,
     inherit.aes = TRUE, ...
 ) {
   layer(
-    geom = GeomPointPie, mapping = mapping,  data = data, stat = stat,
+    geom = GeomPointPie, mapping = mapping,  data = data, stat = StatPointPie,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
     params = list(na.rm = na.rm, r0 = r0, shape = 21, ...)
   )
@@ -81,6 +81,14 @@ GeomPointPie = ggproto(
   },
 
   draw_panel = function(data, panel_params, coord) {
+    munched <- coord_munch(coord, data, panel_params)
+    munched <- munched[order(munched$group), ]
+    if (!is.integer(munched$group)) {
+      munched$group <- match(munched$group, unique0(munched$group))
+    }
+
+    # print(munched)
+
     coords = coord$transform(data, panel_params)
 
     coords = coords %>%
@@ -110,6 +118,40 @@ GeomPointPie = ggproto(
   }
 )
 
+stat_point_pie = function(
+    mapping = NULL, data = NULL, geom = 'point_pie',
+    position = 'identity', n = 360, na.rm = FALSE,
+    show.legend = NA, inherit.aes = TRUE, ...
+  ) {
+  layer(
+    stat = StatPointPie, data = data, mapping = mapping, geom = geom,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, n = n, ...)
+  )
+}
+
+
+#' Calculate the amount for each group
+StatPointPie = ggproto(
+  "StatPointPie", Stat,
+  compute_panel = function(data, scales, m, ...) {
+    data = data %>%
+      group_by(across(everything())) %>%
+      summarize(
+        amount = n(),
+        .groups = "keep"
+      ) %>%
+      group_by(x, y) %>%
+      mutate(
+        theta1 = cumsum(ct),
+        theta0 = theta1 - ct,
+        theta1 = theta1 / sum(ct) * (2 * pi),
+        theta0 = theta0 / sum(ct) * (2 * pi),
+      )
+    print(data)
+    return(data)
+  }
+)
 
 #' Grob to handle a single pie chart.
 #'
@@ -124,37 +166,136 @@ GeomPointPie = ggproto(
 #' When building geom, maybe need to looking into how to deal with the size
 #' and x/y coords.
 #'
+#' New appraoch: consider using the combination of circle and polygon
+#' pointGrob has consistent aspect ratio.
+#' So we take the x, y, and r to make our circle
+#' Mapped to its perimeter point coordinates with `grid::grobPoints()`.
+#' This will be the full circle. Then we map the perimeter point.
+#'
 #' @importFrom grid gpar polygonGrob
 #'
 #' @param ... skip for now
 pieGrob = function(
     x = 0.5, y = 0.5,
-    r0 = 0.1, r1 = 0.3,
+    r0 = 0.0, r1 = 0.3,
     theta0 = 0, theta1 = pi/2,
-    n = 360,
+    n = 360, pch = 21,
     name = NULL,
-    default.units = 'npc',
-    gp = gpar(), vp = NULL
+    position_unit = "npc",
+    size_unit = "snpc",
+    gp = gpar(), vp = NULL,
+    ...
   ) {
 
+  # circles = circleGrob(
+  #   x = x,
+  #   y = y,
+  #   r = r1,
+  #   default.units = default.units,
+  #   vp = vp
+  # )
+  #
+  # poly_points = grobCoords(
+  #   circles,
+  #   closed = TRUE
+  # )
+  # grid::is.unit(poly_points[[1]]$x) %>% print()
+  # xs = lapply(poly_points, function(.x) {.x$x})
+  # ys = lapply(poly_points, function(.x) {.x$y})
+  #
+  # id_length = sapply(xs, length)
+  # xs = unlist(xs)
+  # ys = unlist(ys)
+  # # print(xs)
+  # polygonGrob(
+  #   xs, ys, id.lengths = id_length,
+  #   gp = gp, vp = vp, name = name,
+  #   default.units = "cm"
+  # )
+
+
+  if (!is.unit(x))
+    x <- unit(x, position_unit)
+  if (!is.unit(y))
+    y <- unit(y, position_unit)
+
+  xv <- convertX(x, position_unit, TRUE)
+  yv <- convertY(y, position_unit, TRUE)
+
+
   coords = lapply(seq_len(length(x)), function(i) {
-    .calc_pie(x[i], y[i], r0[i], r1[i], theta0[i], theta1[i], n)
+    .calc_pie(0, 0, r0[i], r1[i], theta0[i], theta1[i], n)
   })
 
   xs = lapply(coords, function(i) {i$x})
   xs = Reduce(c, xs, c())
   ys = lapply(coords, function(i) {i$y})
   ys = Reduce(c, ys, c())
+
+  # Offset with the base xy and yv
+  xs = unit(xv, position_unit) + unit(xs, size_unit)
+  ys = unit(yv, position_unit) + unit(ys, size_unit)
+
   ids_length = lapply(coords, function(i) {length(i$x)})
   ids_length = Reduce(c, ids_length, c())
   polygonGrob(
     x = xs, y = ys, id.lengths = ids_length,
     gp = gp, vp = vp, name = name,
-    default.units = default.units
+    ...
   )
 
 }
 
+
+
+GeomSimpleCircle = ggproto(
+  "GeomSimpleCircle", Geom,
+  draw_panel = function(data, panel_params, coord) {
+    coords = coord$transform(data, panel_params)
+
+    pieGrob(
+      x = coords$x,
+      y = coords$y,
+      r1 = coords$r
+    )
+  }
+)
+
+# polygonGrob(
+#   grobCoords(circleGrob(), closed = TRUE, units = "npc")[[1]]$x,
+#   grobCoords(circleGrob(), closed = TRUE, units = "npc")[[1]]$y
+# ) %>%
+#   grid.draw()
+#
+#
+# lapply(pts, grobPoints, closed = TRUE)
+#
+# grobPoints(pts,closed = TRUE)[[1]]
+#
+#
+# pts = circleGrob(c(0.3, 0.7), c(0.3, 0.7), c(0.1, 0.2))
+
+normalize_vec = function(vec, size_max = 1, size_min = 0) {
+  size_factor = (max(vec) - min(vec)) / (size_max - size_min)
+  vec = (vec - min(vec)) / size_factor + size_min
+  vec
+}
+
+grob_to_ploygon = function(grob_in, outunit = "npc") {
+  poly_points = grobCoords(grob_in, FALSE)
+  # print(str(current.viewport()))
+  xs = lapply(poly_points, function(.x) {.x$x})
+  ys = lapply(poly_points, function(.x) {.x$y})
+
+  id_length = sapply(xs, length)
+  xs = unlist(xs)
+  ys = unlist(ys)
+
+  # xs = normalize_vec(xs)
+  # ys = normalize_vec(ys)
+
+  polygonGrob(xs, ys, id.lengths = id_length, default.units = "cm")
+}
 
 #' Generate coordinates along an arc.
 #' @param .r The radius of the arc
@@ -192,6 +333,7 @@ pieGrob = function(
 #' Generate the coordinate for a single polygonGrob
 .calc_pie = function(.x, .y, .r0, .r1, .theta0, .theta1, n = 360) {
   if (.r0 > 0) {
+    coords = list()
     outer = .calc_arc(.x, .y, .r1, .theta0, .theta1, n)
     inner = .calc_arc(.x, .y, .r0, .theta0, .theta1, n, go_ccw = FALSE)
     coords$x = c(outer$x, inner$x)
--- tests/testthat/test-point_pie.R
+++ tests/testthat/test-point_pie.R
@@ -1,91 +1,154 @@
-test_that(
-  "Test GeomPointPie Object",
-  code = {
-    iris %>%
-      ggplot(aes(x = Sepal.Length, y = Sepal.Width)) +
-      geom_point_pie(aes(color = Species))
-  }
-)
+# test_that(
+#   "Test GeomPointPie Object",
+#   code = {
+#     iris %>%
+#       ggplot(aes(x = Sepal.Length, y = Sepal.Width)) +
+#       geom_point_pie(aes(color = Species))
+#   }
+# )
+#
+#
+#
+# test_that("Test pieGrob", {
+#   out = pieGrob(
+#     x = c(0.5, 0.5),
+#     y = c(0.5, 0.5),
+#     r0 = c(0, 0),
+#     r1 = c(0.3, 0.3),
+#     theta0 = c(0, pi),
+#     theta1 = c(pi,2 * pi),
+#     gp = gpar(fill = c("red", "blue"))
+#   )
+#   grid.draw(out)
+# }
+# )
+#
+# test_that("Basic geom_pie_point", {
+#   dat = tibble::tibble(
+#     x = rep(1, 5),
+#     y = rep(1, 5)
+#   ) %>%
+#     dplyr::mutate(
+#       r_idx = dplyr::row_number(),
+#       grp = sample(1:10, 5),
+#       grp = map2(grp, r_idx, ~rep(letters[.y], times = .x))
+#     ) %>%
+#     tidyr::unnest(grp)
+#   dat %>%
+#     ggplot(aes(x = x, y = y)) +
+#     geom_point_pie(aes(group = grp, fill = grp))
+#
+#
+#   dat2 = tibble::tibble(
+#     x = 1:5,
+#     y = 1:5
+#   ) %>%
+#     mutate(
+#       data = map(x, ~select(dat, -c("x", "y")))
+#     ) %>%
+#     tidyr::unnest(data)
+#
+#   dat2 %>%
+#     mutate(x = as.character(x), size = normalize_size(y)) %>%
+#     ggplot(aes(x = x, y = y)) +
+#     geom_point_pie(aes(fill = grp, size = size, linetype = y == 2)) +
+#     scale_fill_viridis_d() +
+#     scale_x_discrete(expand = expansion(add = 1)) +
+#     scale_y_continuous(expand = expansion(add = 1)) +
+#     scale_size_continuous(range = c(0.05, 0.30)) +
+#     facet_wrap(
+#       vars(x),
+#       scales = "free"
+#     ) +
+#     guides(
+#       linetype = guide_legend(
+#         override.aes = list(fill = NA)
+#       ),
+#       size = guide_legend(
+#         override.aes = list(fill = NA)
+#       )
+#     )
+#
+#   dat2 %>%
+#     mutate(x = as.character(x), size = normalize_size(y)) %>%
+#     ggplot(aes(x = x, y = y)) +
+#     geom_point(size = 20)  +
+#     facet_wrap(
+#       vars(x),
+#       scales = "free"
+#     ) +
+#     scale_y_discrete(limits = c(4, 6))
+#
+#   tibble::tibble(
+#     x = letters[1:5],
+#     y = letters[1:5]
+#   ) %>%
+#     mutate(
+#       data = map(x, ~select(dat, -c("x", "y")))
+#     ) %>%
+#     tidyr::unnest(data) %>%
+#     mutate(x = as.character(x)) %>%
+#     ggplot(aes(x = x, y = y)) +
+#     geom_point_pie(aes(fill = grp, linetype = y == "a")) +
+#     # scale_fill_viridis_d() +
+#     # scale_x_discrete(expand = expansion(add = 1)) +
+#     # scale_y_continuous(expand = expansion(add = 1)) +
+#     # scale_size_continuous(range = c(0.05, 0.10)) +
+#     facet_wrap(
+#       vars(x),
+#       scales = "free"
+#     ) +
+#     guides(
+#       linetype = guide_legend(
+#         override.aes = list(fill = NA)
+#       ),
+#       size = guide_legend(
+#         override.aes = list(fill = NA)
+#       )
+#     )
+# })
 
 
+test_that("Check circlGrob and pieGrob look the same", {
+  xs = c(0.3, 0.7)
+  ys = c(0.3, 0.7)
+  rs = c(0.1, 0.3)
 
-test_that("Test pieGrob", {
-  out = pieGrob(
-    x = c(0.5, 0.5),
-    y = c(0.5, 0.5),
-    r0 = c(0, 0),
-    r1 = c(0.3, 0.3),
-    theta0 = c(0, pi),
-    theta1 = c(pi,2 * pi),
-    gp = gpar(fill = c("red", "blue"))
-  )
-  grid.draw(out)
-}
-)
+  vdiffr::expect_doppelganger("pointGrob", grid.draw(circleGrob(xs, ys, rs)))
 
-test_that("Basic geom_pie_point", {
-  dat = tibble::tibble(
-    x = rep(1, 5),
-    y = rep(1, 5)
-  ) %>%
-    dplyr::mutate(
-      r_idx = dplyr::row_number(),
-      grp = sample(1:10, 5),
-      grp = map2(grp, r_idx, ~rep(letters[.y], times = .x))
-    ) %>%
-    tidyr::unnest(grp)
-  dat %>%
-    ggplot(aes(x = x, y = y)) +
-    geom_point_pie(aes(group = grp, fill = grp))
-
-
-  dat2 = tibble::tibble(
-    x = 1:5,
-    y = 1:5
-  ) %>%
-    mutate(
-      data = map(x, ~select(dat, -c("x", "y")))
-    ) %>%
-    tidyr::unnest(data)
-
-  dat2 %>%
-    mutate(x = as.character(x), size = normalize_size(y)) %>%
-    ggplot(aes(x = x, y = y)) +
-    geom_point_pie(aes(fill = grp, size = size, linetype = y == 2)) +
-    scale_fill_viridis_d() +
-    scale_x_discrete(expand = expansion(add = 1)) +
-    scale_y_continuous(expand = expansion(add = 1)) +
-    scale_size_continuous(range = c(0.05, 0.10)) +
-    facet_wrap(
-      vars(x),
-      scales = "free"
-    ) +
-    guides(
-      linetype = guide_legend(
-        override.aes = list(fill = NA)
-      ),
-      size = guide_legend(
-        override.aes = list(fill = NA)
-      )
-    )
-
-  tibble::tibble(
-    x = letters[1:5],
-    y = letters[1:5]
-  ) %>%
-    mutate(
-      data = map(x, ~select(dat, -c("x", "y")))
-    ) %>%
-    tidyr::unnest(data) %>%
-    mutate(x = as.character(x), size = y) %>%
-    ggplot(aes(x = x, y = y)) +
-    geom_point_pie(aes(fill = grp, size = size, linetype = y == "a")) +
-    # scale_fill_viridis_d() +
-    # scale_x_discrete(expand = expansion(add = 1)) +
-    # scale_y_continuous(expand = expansion(add = 1)) +
-    # scale_size_continuous(range = c(0.05, 0.10)) +
-    facet_wrap(
-      vars(x),
-      scales = "free"
-    )
+  vdiffr::expect_doppelganger("pieGrob", grid.draw(pieGrob(xs, ys, r1 = rs, theta0 = 0, theta1 = 2 * pi)))
 })
+
+
+
+dat = tibble::tibble(
+  x = rep(1, 5),
+  y = rep(1, 5)
+  ) %>%
+  dplyr::mutate(
+    r_idx = dplyr::row_number(),
+    grp = sample(1:10, 5),
+    grp = map2(grp, r_idx, ~rep(letters[.y], times = .x))
+  ) %>%
+  tidyr::unnest(grp)
+
+
+tibble::tibble(
+  x = letters[1:5],
+  y = letters[1:5],
+  r = 1:5 / 10,
+  # x = 1:5,
+  # y = 1:5
+) %>%
+  mutate(
+    data = map(x, ~select(dat, -c("x", "y")))
+  ) %>%
+  tidyr::unnest(data) %>%
+  group_by(across(everything())) %>%
+  summarise(amount = n()) %>%
+  ungroup() %>%
+  ggplot(aes()) +
+  layer(
+    geom = GeomPointPie, stat = StatIdentity, mapping = aes(x = x, y = y, r1 = 0.1), position = PositionIdentity
+  ) +
+  facet_wrap(vars(x), ncol = 1, scales = "free")
